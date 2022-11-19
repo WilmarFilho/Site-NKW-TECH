65aeb49df340d9784e72d3a216c5f135982d5e7d
Wilmar Filho <wilmarfilho32@hotmail.com>
2022-11-19 16:47:06 -0300

    Corrrigindo CORS

--- app/Http/Controllers/PagamentoController.php
+++ app/Http/Controllers/PagamentoController.php
@@ -25,6 +25,7 @@ class PagamentoController extends Controller
             \Stripe\Stripe::setApiKey('sk_test_51LsYCqHCT0bYlJFqcTG8ZP72VfWqIZEgAJlHFUvCMDDf76b2OtYROKo8r2LXBTiGaHYe0o9qGLVNQ5x5Zw3iE8k200OWvffTaE');
 
             header('Content-Type: application/json');
+            
 
             $YOUR_DOMAIN = 'http://127.0.0.1:8000';
 
@@ -59,6 +60,8 @@ class PagamentoController extends Controller
     public function pagamentoSucesso() {
 
         User::where('id', auth()->user()->id)->update(['assinatura' => 'premium']);
+
+        header('Access-Control-Allow-Origin: https://checkout.stripe.com/c/pay/cs_test_a1dEDKpPh7LdfX8M4EFoS1GWVQVatVy2AYKQ0MMHXmsJAIMONp1nkZrNdg#fidkdWxOYHwnPyd1blpxYHZxWjA0SXZcRnRNRlE1Z1xpT0N0aklmfTBOd0hjX1xHN3JrYDRtMWxIZ0dtcWhUaEtiQXZBTWJqZjRENkMxRkEyUTxTZDNzRmhzTVJKMV82dDM0RGdjPXxXMVdVNTVRUVYya05BRicpJ2N3amhWYHdzYHcnP3F3cGApJ2lkfGpwcVF8dWAnPyd2bGtiaWBabHFgaCcpJ2BrZGdpYFVpZGZgbWppYWB3dic%2FcXdwYHgl');
 
         return redirect()->route('home');
 
